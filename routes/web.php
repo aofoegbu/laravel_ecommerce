d2baad74ff7b4f5f574b77451ba453c4c1b28a72
Augustine Ofoegbu <augustineofoegbu1@gmail.com>
2021-03-14 16:17:40 +0100

    Project structure, shop page/feature, user/admin login features and fetching data from database complete

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 
@@ -31,6 +32,8 @@ Route::get('/cart', CartComponent::class);
 
 
 Route::get('/checkout', CheckoutComponent::class);
+
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
